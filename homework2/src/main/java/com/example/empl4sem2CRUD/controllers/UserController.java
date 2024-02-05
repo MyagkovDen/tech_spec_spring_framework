3880ead4d60658acf1efdaf68bf5110f9d737ad0
Denis Miagkov <tomich-84@mail.ru>
2024-02-05 16:51:46 +0300

    homework 2: done

---
+++ homework2/src/main/java/com/example/empl4sem2CRUD/controllers/UserController.java
@@ -0,0 +1,57 @@
+package com.example.empl4sem2CRUD.controllers;
+
+import com.example.empl4sem2CRUD.model.User;
+import com.example.empl4sem2CRUD.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String findAll(Model model) {
+        List<User> users = userService.findAll();
+
+
+        model.addAttribute("users", users);
+        return "user-list";
+        //return "home.html";
+    }
+
+    @GetMapping("/user-create")
+    public String createUserForm(User user) {
+        return "user-create";
+    }
+
+    @PostMapping("/user-create")
+    public String createUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("user-delete/{id}")
+    public String deleteUser(@PathVariable int id) {
+        userService.deleteById(id);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/user-update/{id}")
+    public String getOne(@PathVariable("id") int id, Model model){
+        User user = userService.getOne(id);
+        model.addAttribute("user", user);
+        return "user-update";
+    }
+    @PostMapping("user-update")
+    public String updateUser(User user) {
+        userService.updateUser(user);
+        return "redirect:/users";
+    }
+}
