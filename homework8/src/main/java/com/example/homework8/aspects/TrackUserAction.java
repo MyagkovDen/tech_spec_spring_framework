d51f4fab96a12cd457e9fa9bee304dcdf97f0761
Denis Miagkov <tomich-84@mail.ru>
2024-03-04 16:08:02 +0300

    homework8: done

---
+++ homework8/src/main/java/com/example/homework8/aspects/TrackUserAction.java
@@ -0,0 +1,11 @@
+package com.example.homework8.aspects;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface TrackUserAction {
+}
---
+++ homework8/src/main/java/com/example/homework8/repository/TaskRepository.java
@@ -0,0 +1,43 @@
+package com.example.homework8.repository;
+
+import com.example.homework8.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    /**
+     * Добавление задачи
+     */
+    Task save(Task task);
+
+    /**
+     * Просмотр всех задач
+     */
+    List<Task> findAll();
+
+    /**
+     * Просмотр задач по статусу
+     */
+    List<Task> findByStatus(Task.TaskStatus status);
+
+    /**
+     * Изменение статуса задачи
+     */
+    @Transactional
+    @Modifying
+    @Query("update Task t " +
+           "set t.status = :status " +
+           "where t.id = :id")
+    int updateTaskStatus(Task.TaskStatus status, Long id);
+
+    /**
+     * Удаление задачи
+     */
+    @Transactional
+    void deleteTaskById(Long id);
+
+}
